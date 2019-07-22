1bff392c8aaa9a916347cb78d4468e482ee262b7
Davidodari <davidkibzodari@gmail.com>
2019-07-22 19:57:34 +0300

    Add source files

---
+++ linked_list.c
@@ -0,0 +1,5 @@
+//
+// Created by blackcoder on 22/07/19.
+//
+
+#include "linked_list.h"
---
+++ linked_list.h
@@ -0,0 +1,8 @@
+//
+// Created by blackcoder on 22/07/19.
+//
+
+#ifndef LIST_LINKED_LIST_H
+#define LIST_LINKED_LIST_H
+
+#endif //LIST_LINKED_LIST_H
