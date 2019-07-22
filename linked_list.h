f83eae2208d29f7c6512d7195192816ea7940be2
Davidodari <davidkibzodari@gmail.com>
2019-07-22 22:48:17 +0300

    define header

--- linked_list.h
+++ linked_list.h
@@ -1,8 +1,30 @@
 #ifndef LIST_LINKED_LIST_H
 #define LIST_LINKED_LIST_H
 
-struct node{
-
+struct node {
+  struct node* next;
 };
+
+/**
+ *
+ * Takes in new group member data and appends the node to the list
+ *
+ */
+void add();
+
+/**
+ *
+ * Remove node pointing to a group member from the list
+ *
+ */
+void remove();
+
+
+/**
+ *
+ * Display data in the list
+ *
+ */
+void list();
 
 #endif
